f5c2dd0f0d171d569ccf16904b153e5477c93128
Clément Dufaure <clement.dufaure@insee.fr>
2022-05-21 00:51:01 +0200

    a

--- application-basique/application-basique-oidc/src/main/java/fr/insee/demo/controller/Controlleur.java
+++ application-basique/application-basique-oidc/src/main/java/fr/insee/demo/controller/Controlleur.java
@@ -1,5 +1,8 @@
 package fr.insee.demo.controller;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.pac4j.core.context.JEEContext;
 import org.pac4j.core.context.WebContext;
 import org.pac4j.core.context.session.JEESessionStore;
@@ -7,6 +10,7 @@ import org.pac4j.core.matching.matcher.csrf.DefaultCsrfTokenGenerator;
 import org.pac4j.core.profile.ProfileManager;
 import org.pac4j.core.profile.UserProfile;
 import org.pac4j.core.util.Pac4jConstants;
+import org.pac4j.oidc.profile.OidcProfile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -15,94 +19,78 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Optional;
 
 @Controller
 public class Controlleur {
-
-  Logger log = LoggerFactory.getLogger(Controlleur.class);
-
-  @RequestMapping(value = { "/accueil", "/" })
-  public String accueil(Model model) {
-    return "accueil";
-  }
-
-  @RequestMapping(value = { "/private", "/admin" })
-  public String espacePrivate(HttpServletRequest request, HttpServletResponse response, Model model) {
-
-    model.addAttribute("secure", request.isSecure());
-
-    String modeAuthentification = "";
-    if (request.getUserPrincipal() == null) {
-      modeAuthentification = "Authentifié par ... pas d'authentification en fait";
-    } else {
-      modeAuthentification = "Vous êtes bien authentifié";
-    }
-
-    model.addAttribute("typeAuthentification", modeAuthentification);
-
-    /*
-     * Pour récupérer des informations sur l'utilisateur avec un adapter keycloak :
-     * 
-     * KeycloakSecurityContext sc = (KeycloakSecurityContext)
-     * request.getAttribute(KeycloakSecurityContext.class.getName());
-     * 
-     * 
-     */
-
-    WebContext context = new JEEContext(request, response);
-    ProfileManager manager = new ProfileManager(context,JEESessionStore.INSTANCE);
-    Optional<UserProfile> profile = manager.getProfile();
-    Map h = profile.get().getAttributes();
-
-    model.addAttribute("nom", h.get("name"));
-    model.addAttribute("mail", h.get("email"));
-    model.addAttribute("roles", h.get("roles"));
-    model.addAttribute("urlAccount", "https://mon.serveur.keycloak/auth/realms/formation/account"
-        + "?referrer=client-test-web&referrer_uri=https://localhost:8443/private");
-
-    var gen= new DefaultCsrfTokenGenerator();
-    var csrfToken = gen.get(context,JEESessionStore.INSTANCE);
-    model.addAttribute("_csrf_token_name", Pac4jConstants.CSRF_TOKEN );
-    model.addAttribute("_csrf_token", csrfToken);
-
-    /* Envoi d'une requête au ws */
-
-//    HttpURLConnection connection = null;
-//    try {
-//      // Create connection
-//      URL url = new URL(
-//          "https://mon.serveur.keycloak/auth/realms/formation/protocol/openid-connect/userinfo");
-//      connection = (HttpURLConnection) url.openConnection();
-//      connection.setRequestMethod("GET");
-//
-//      // Get Response
-//      InputStream is = null;
-//      try {
-//        is = connection.getInputStream();
-//      } catch (IOException e) {
-//        is = connection.getErrorStream();
-//      }
-//
-//      BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-//      StringBuffer response = new StringBuffer();
-//      String line;
-//      while ((line = rd.readLine()) != null) {
-//        response.append(line);
-//        response.append('\r');
-//      }
-//      rd.close();
-//      model.addAttribute("reponseWS", response.toString());
-//    } catch (Exception e) {
-//      e.printStackTrace();
-//    } finally {
-//      if (connection != null) {
-//        connection.disconnect();
-//      }
-//    }
-
-    return "private";
-  }
-
+	
+	Logger log = LoggerFactory.getLogger(Controlleur.class);
+	
+	private String webServiceUrl =
+			  "http://localhost:8180/auth/realms/test/protocol" +
+						 "/openid-connect/userinfo";
+	
+	private String urlAccount =
+			  "http://localhost:8180/auth/realms/formation/account?referrer" +
+						 "=client-test-web&referrer_uri=https://localhost:8443" +
+						 "/private";
+	
+	@RequestMapping(value = {"/accueil", "/"})
+	public String accueil(Model model) {
+		return "accueil";
+	}
+	
+	@RequestMapping(value = {"/private", "/admin"})
+	public String espacePrivate(HttpServletRequest request,
+										 HttpServletResponse response, Model model)
+			  throws IOException {
+		
+		
+		model.addAttribute("secure", request.isSecure());
+		
+		String modeAuthentification = "";
+		if (request.getUserPrincipal() == null) {
+			modeAuthentification =
+					  "Authentifié par ... pas d'authentification en fait";
+		} else {
+			modeAuthentification = "Vous êtes bien authentifié";
+		}
+		
+		model.addAttribute("typeAuthentification", modeAuthentification);
+		
+		WebContext context = new JEEContext(request, response);
+		ProfileManager manager =
+				  new ProfileManager(context, JEESessionStore.INSTANCE);
+		Optional<UserProfile> profile = manager.getProfile();
+		OidcProfile oidcProfile = (OidcProfile) profile.get();
+		
+		Map h = profile.get().getAttributes();
+		
+		model.addAttribute("nom", h.get("name"));
+		model.addAttribute("mail", h.get("email"));
+		model.addAttribute("roles", oidcProfile.getRoles());
+		model.addAttribute("urlAccount", urlAccount);
+		
+		var gen = new DefaultCsrfTokenGenerator();
+		var csrfToken = gen.get(context, JEESessionStore.INSTANCE);
+		model.addAttribute("_csrf_token_name", Pac4jConstants.CSRF_TOKEN);
+		model.addAttribute("_csrf_token", csrfToken);
+		
+		/* Envoi d'une requête au ws */
+		
+		OkHttpClient client = new OkHttpClient();
+		
+		Request req = new Request.Builder().url(webServiceUrl).get()
+				  .header("Authorization",
+							 oidcProfile.getAccessToken().toAuthorizationHeader())
+				  .build();
+		
+		Response rep = client.newCall(req).execute();
+		model.addAttribute("reponseWS", rep.body().string());
+		
+		return "private";
+	}
+	
 }
--- application-basique/application-basique-oidc/src/main/java/fr/insee/demo/security/DemoConfigFactory.java
+++ application-basique/application-basique-oidc/src/main/java/fr/insee/demo/security/DemoConfigFactory.java
@@ -5,48 +5,63 @@ import org.pac4j.core.authorization.authorizer.IsAuthenticatedAuthorizer;
 import org.pac4j.core.authorization.authorizer.RequireAnyRoleAuthorizer;
 import org.pac4j.core.config.Config;
 import org.pac4j.core.config.ConfigFactory;
-import org.pac4j.core.http.url.DefaultUrlResolver;
 import org.pac4j.core.matching.matcher.PathMatcher;
+import org.pac4j.oidc.authorization.generator.KeycloakRolesAuthorizationGenerator;
 import org.pac4j.oidc.client.OidcClient;
 import org.pac4j.oidc.config.OidcConfiguration;
+import org.pac4j.oidc.profile.OidcProfileDefinition;
+import org.pac4j.oidc.profile.creator.OidcProfileCreator;
+import org.pac4j.oidc.profile.keycloak.KeycloakOidcProfile;
 
 public class DemoConfigFactory implements ConfigFactory {
-
-
-    private String configurationEndpoint = "http://localhost:8180/auth/realms/test/.well-known/openid-configuration";
-    public String clientId = "localhost-java";
-    public String clientSecret = "0756132b-0e7a-425c-a0e3-7d5946fd372c";
-
-    @Override
-    public Config build(final Object... parameters) {
-
-        OidcConfiguration oidcConfig = new OidcConfiguration();
-        oidcConfig.setClientId(clientId);
-        oidcConfig.setSecret(clientSecret);
-        oidcConfig.setDiscoveryURI(configurationEndpoint);
-        OidcClient oidcClient = new OidcClient(oidcConfig);
-
-        // accept relative urls in callback definition
-        // WARN : the explicit tomcat context is required
-//        oidcClient.setCallbackUrl("demo/callback");
-//        DefaultUrlResolver resolver = new DefaultUrlResolver();
-//        resolver.setCompleteRelativeUrl(true);
-
-        oidcClient.setCallbackUrl("/callback");
-        RelativeUrlResolver resolver = new RelativeUrlResolver();
-        resolver.setCompleteRelativeUrl(true);
-        oidcClient.setUrlResolver(resolver);
-
-        Config config = new Config(oidcClient);
-        config.addAuthorizer("admin", new RequireAnyRoleAuthorizer("ROLE_ADMIN"));
-        config.addAuthorizer("mustBeAuthent",new IsAuthenticatedAuthorizer());
-        config.addAuthorizer("csrfToken",new CsrfAuthorizer());
-
-        config.addMatcher("excludedPath", new PathMatcher().excludeRegex("^\\/(accueil)?$"));
-
-        config.setLogoutLogic(new RelativeLogoutLogic());
-
-        return config;
-    }
-
+	
+	
+	public String clientId = "localhost-java";
+	public String clientSecret = "0756132b-0e7a-425c-a0e3-7d5946fd372c";
+	private String configurationEndpoint =
+			  "http://localhost:8180/auth/realms/test/" +
+						 ".well-known/openid-configuration";
+	
+	@Override
+	public Config build(final Object... parameters) {
+		
+		OidcConfiguration oidcConfig = new OidcConfiguration();
+		oidcConfig.setClientId(clientId);
+		oidcConfig.setSecret(clientSecret);
+		oidcConfig.setDiscoveryURI(configurationEndpoint);
+		OidcClient oidcClient = new OidcClient(oidcConfig);
+		
+		// accept relative urls in callback definition
+		// WARN : the explicit tomcat context is required
+		//        oidcClient.setCallbackUrl("demo/callback");
+		//        DefaultUrlResolver resolver = new DefaultUrlResolver();
+		//        resolver.setCompleteRelativeUrl(true);
+		
+		oidcClient.setCallbackUrl("/callback");
+		RelativeUrlResolver resolver = new RelativeUrlResolver();
+		resolver.setCompleteRelativeUrl(true);
+		oidcClient.setUrlResolver(resolver);
+		
+		// Transform default profile in Keycloak Profile and select how to get
+		// roles
+		var profileCreator = new OidcProfileCreator(oidcConfig, oidcClient);
+		profileCreator.setProfileDefinition(
+				  new OidcProfileDefinition(x -> new KeycloakOidcProfile()));
+		oidcClient.setProfileCreator(profileCreator);
+		oidcClient.addAuthorizationGenerator(
+				  new KeycloakRolesAuthorizationGenerator());
+		
+		Config config = new Config(oidcClient);
+		config.addAuthorizer("admin", new RequireAnyRoleAuthorizer("admin"));
+		config.addAuthorizer("mustBeAuthent", new IsAuthenticatedAuthorizer());
+		config.addAuthorizer("csrfToken", new CsrfAuthorizer());
+		
+		config.addMatcher("excludedPath",
+				  new PathMatcher().excludeRegex("^\\/(accueil)?$"));
+		
+		config.setLogoutLogic(new RelativeLogoutLogic());
+		
+		return config;
+	}
+	
 }
