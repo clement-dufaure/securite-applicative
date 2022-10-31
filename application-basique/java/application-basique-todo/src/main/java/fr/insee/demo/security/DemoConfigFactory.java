c8fc361ddadfec556b9e96d5dc58bf96641f8faa
Clément Dufaure <clement.dufaure@insee.fr>
2022-10-31 17:34:41 +0100

    ajustements js

--- application-basique/java/application-basique-todo/src/main/java/fr/insee/demo/security/DemoConfigFactory.java
+++ application-basique/java/application-basique-todo/src/main/java/fr/insee/demo/security/DemoConfigFactory.java
@@ -5,8 +5,7 @@ public class DemoConfigFactory  {
 	
 	public String clientId = "localhost-java";
 	public String clientSecret = "";
-	private String configurationEndpoint =
-			  "http://localhost:8180/auth/realms/test/" +
+	private String configurationEndpoint = "http://localhost:8180/auth/realms/test/" +
 						 ".well-known/openid-configuration";
 	
 	
