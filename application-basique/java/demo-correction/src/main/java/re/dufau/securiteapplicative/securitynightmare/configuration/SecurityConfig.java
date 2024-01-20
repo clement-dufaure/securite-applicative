da8aaad7c5e789b22cd33a2130e86fd338da12ae
Clément Dufaure <clement.dufaure@insee.fr>
2024-01-20 22:50:44 +0100

    2024

--- application-basique/java/demo-correction/src/main/java/re/dufau/securiteapplicative/securitynightmare/configuration/SecurityConfig.java
+++ application-basique/java/demo-correction/src/main/java/re/dufau/securiteapplicative/securitynightmare/configuration/SecurityConfig.java
@@ -1,21 +1,30 @@
 package re.dufau.securiteapplicative.securitynightmare.configuration;
 
+import com.nimbusds.jwt.JWT;
+import com.nimbusds.jwt.JWTParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
-import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @Configuration
@@ -24,18 +33,18 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception {
-        //http.oauth2Login(Customizer.withDefaults());
-        http.oauth2Login((oauth2Login) -> oauth2Login
-                        .userInfoEndpoint((userInfo) -> userInfo
-                                .userAuthoritiesMapper(grantedAuthoritiesMapper())
-                        ));
-        // TODO : roles
+//        http.oauth2Login(Customizer.withDefaults());
+        http.oauth2Login((oauth2Login) -> oauth2Login.userInfoEndpoint((userInfo) -> userInfo
+                .oidcUserService(oidcUserService())));
         http.logout((logout) -> logout
                 .logoutSuccessHandler(oidcLogoutSuccessHandler())
         );
-        http.authorizeHttpRequests(authorize -> authorize.requestMatchers(HttpMethod.GET, "/forum/**").authenticated()
-                .requestMatchers(HttpMethod.GET, "/").permitAll()
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.GET, "/", "css/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/forum/**").authenticated()
+                .requestMatchers(HttpMethod.POST, "/forum/**").authenticated()
                 .requestMatchers(HttpMethod.GET, "/admin/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/admin/**").hasRole("ADMIN")
                 .anyRequest().denyAll());
         return http.build();
     }
@@ -46,44 +55,36 @@ public class SecurityConfig {
     private LogoutSuccessHandler oidcLogoutSuccessHandler() {
         OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler =
                 new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
-
-
         // Sets the location that the End-User's User Agent will be redirected to
         // after the logout has been performed at the Provider
         oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
-
         return oidcLogoutSuccessHandler;
     }
 
 
-    //https://docs.spring.io/spring-security/reference/5.8/migration/servlet/oauth2.html
-    private GrantedAuthoritiesMapper grantedAuthoritiesMapper() {
-        return (authorities) -> {
-            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+    private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+        final OidcUserService delegate = new OidcUserService();
 
-            authorities.forEach((authority) -> {
-                GrantedAuthority mappedAuthority;
-
-                if (authority instanceof OidcUserAuthority) {
-                    OidcUserAuthority userAuthority = (OidcUserAuthority) authority;
-                    mappedAuthority = new OidcUserAuthority(
-                            "OIDC_USER", userAuthority.getIdToken(), userAuthority.getUserInfo());
-                } else if (authority instanceof OAuth2UserAuthority) {
-                    OAuth2UserAuthority userAuthority = (OAuth2UserAuthority) authority;
-                    mappedAuthority = new OAuth2UserAuthority(
-                            "OAUTH2_USER", userAuthority.getAttributes());
-                } else {
-                    mappedAuthority = authority;
+        return (userRequest) -> {
+            // Delegate to the default implementation for loading a user
+            OidcUser oidcUser = delegate.loadUser(userRequest);
+            OAuth2AccessToken accessToken = userRequest.getAccessToken();
+            try {
+                JWT jwt = JWTParser.parse(accessToken.getTokenValue());
+                Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+                var realmAcess = (Map<String, List<String>>) jwt.getJWTClaimsSet().getClaims().get("realm_access");
+                var roles = realmAcess.get("roles");
+                for (var role : roles) {
+                    mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + role.toUpperCase()));
                 }
-
-                mappedAuthorities.add(mappedAuthority);
-            });
-
-            return mappedAuthorities;
+                oidcUser = new DefaultOidcUser(mappedAuthorities, oidcUser.getIdToken(), oidcUser.getUserInfo(), "preferred_username");
+            } catch (Exception e) {
+                //maybe throw sth intercept as an User Exception
+                throw new UnsupportedOperationException();
+            }
+            return oidcUser;
         };
-
-
-
+    }
 
 
 }
